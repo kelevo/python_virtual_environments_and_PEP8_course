d6e052a46bd9999f4090167ba13cc593cc01c9e1
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2025-11-16 21:55:56 -0600

    f strings

---
+++ fstrings.py
@@ -0,0 +1,17 @@
+name = "Ana"
+year = 2020
+text = f"Hola {name}"
+print(text)
+
+
+text_suma = f"Hola {name}, tu edad es: {2025 - year}"
+print(text_suma)
+
+
+text_function = f"HOLA {name.upper()}"
+print(text_function)
+
+
+edad = 20
+text_if = f"Hola {name}, eres {'mayor' if edad >= 18 else 'menor'} de edad"
+print(text_if)
