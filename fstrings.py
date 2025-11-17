286cd9e83dc6960d1af148928507b0e296fca834
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2025-11-16 22:12:36 -0600

    f string for numbers and dates

--- fstrings.py
+++ fstrings.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 name = "Ana"
 year = 2020
 text = f"Hola {name}"
@@ -15,3 +17,30 @@ print(text_function)
 edad = 20
 text_if = f"Hola {name}, eres {'mayor' if edad >= 18 else 'menor'} de edad"
 print(text_if)
+
+
+bank_balance = 1200000000
+text_bank_balance = f"Tu saldo en la cuenta bancaria es: {bank_balance:,}"
+print(text_bank_balance)
+
+
+stock_price = 1.405
+text_stock_price = f"El valor del stock es: {stock_price:.1f}"
+print(text_stock_price)
+
+
+user_id = 1
+text_user_id = f"Su id es: {user_id:03d}"
+print(text_user_id)
+
+
+product = "Laptop"
+price = 1000
+text_product = f"Producto: {product:>15} | Precio: {price}"
+print(text_product)
+print(f"{text_product}\n{text_product}")
+
+
+date = datetime(2024, 12, 5, 10, 10)
+text_datetime = f"La fecha es: {date: %A %d de %Y a las %I:%M %p}"
+print(text_datetime)
